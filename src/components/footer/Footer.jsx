af49ae6da7a0e759583f20abf8c7b966f9225e21
jzpoe <pmguez1287@gmail.com>
2024-04-11 17:26:49 -0500

    responsive

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,40 +11,40 @@ const Footer = () => {
           <p>Una breve descripción de tu empresa o sitio web.</p>
         </div> */}
         
-        <div className="footer-section links">
+        <div className="footer-section-links">
           
           <ul>
-          <div className="footer-section contact">
+          <div className="footer-section-contact">
           <h2 className="footer-section-title">Contactanos</h2>
-          <p>Dirección: Calle, Ciudad, País</p>
-          <p>Teléfono: +123456789</p>
-          <p>Email: info@ejemplo.com</p>
+          <p className='clase-p2'>Dirección: Calle, Ciudad, País</p>
+          <p className='clase-p'>Teléfono: +123456789</p>
+          <p className='clase-p'>Email: info@ejemplo.com</p>
         </div>
             <li> <a href="mailto:pmguez1287@gmail.com">
-              <img className='img-gmail' src="img/footer/gmail.jpg" alt="imagen de correo" />
-    
+                {/* <img className='img-gmail' src="img/footer/gmail.jpg" alt="imagen de correo" />
+      */}
   </a> </li>
             
            
            
           </ul>
         </div>
-        <div className="footer-section social">
+        <div className="footer-section-social">
           <h2 className="footer-section-title">Redes Sociales</h2>
           <ul>
           <li><a href="mailto:pmguez1287@gmail.com">
-              <img className='img-gmail' src="img/footer/facebook.jpg" alt="imagen de correo" />
-    
+              {/* <img className='img-gmail' src="img/footer/facebook.jpg" alt="imagen de correo" />
+     */}
   </a> </li>
-            <li><a href="https://twitter.com/ejemplo"><i className="fab fa-twitter"></i></a></li>
-            <li><a href="https://www.instagram.com/ejemplo"><i className="fab fa-instagram"></i></a></li>
-            <li><a href="https://www.linkedin.com/company/ejemplo"><i className="fab fa-linkedin"></i></a></li>
+  <li><a href="https://twitter.com/ejemplo"><i className="fab fa-twitter"></i></a></li>
+<li><a href="https://www.instagram.com/ejemplo"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg></a></li>
+<li><a href="https://www.linkedin.com/company/ejemplo"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M512 256C512 114.6 397.4 0 256 0S0 114.6 0 256C0 376 82.7 476.8 194.2 504.5V334.2H141.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H287V510.1C413.8 494.8 512 386.9 512 256h0z"/></svg></a></li>
           </ul>
         </div>
       </div>
       <div className="footer-bottom">
-        <p>Derechos de autor &copy; {new Date().getFullYear()} Ejemplo.com. Todos los derechos reservados.</p>
-        <p><a href="/privacidad">Política de Privacidad</a> | <a href="/terminos">Términos de Servicio</a></p>
+        <p className='clase-p1'>Derechos de autor &copy; {new Date().getFullYear()} Ejemplo.com. Todos los derechos reservados.</p>
+        <p className='clase-p1'><a href="/privacidad">Política de Privacidad</a> | <a href="/terminos">Términos de Servicio</a></p>
       </div>  
     </footer>
   );
