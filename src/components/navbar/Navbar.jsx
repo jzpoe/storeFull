e1955896bf2aa4bcf8f8a7817e46fefe1e09f20f
jzpoe <pmguez1287@gmail.com>
2024-04-15 17:58:30 -0500

    se da un nuevo estilo y responsive 2

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -19,52 +19,50 @@ export const Navbar = () => {
 
   return (
     <>
-    <div className="logo">
-    <h1>
-  <span className="clas-spam">•M</span>
-  <span >y</span>
-  <span className="class-span" >S</span>tore
-  <span className="class-span" >s</span>•
+      <div className="logo">
+        <h1>
+          <span className="clas-spam">•M</span>
+          <span>y</span>
+          <span className="class-span">S</span>tore
+          <span className="class-span">s</span>•
+        </h1>
+      </div>
 
-
-</h1>  </div>
-    
-    <nav className="container-navbar">
-  
-  <div className="menu-icon" onClick={() => setIsOpen(!isOpen)}>
-    <GiHamburgerMenu />
-  </div>
-  <ul className={`ul-navbar ${isOpen ? "open" : ""}`}>
-    <li className="navbar-li">
-      <Link to="/">
-        {" "}
-        <IoMan />
-      </Link>
-    </li>
-    {/* Comenté los elementos que no deseas mostrar */}
-    {/* <li className="navbar-li">
+      <nav className="container-navbar">
+        <div className="menu-icon" onClick={() => setIsOpen(!isOpen)}>
+          <GiHamburgerMenu  className="menu-hamburguesa"/>
+        </div>
+        <ul className={`ul-navbar ${isOpen ? "open" : ""}`}>
+          <li className="navbar-li">
+            <Link to="/">
+              {" "}
+              <IoMan />
+            </Link>
+          </li>
+          {/* Comenté los elementos que no deseas mostrar */}
+          {/* <li className="navbar-li">
       <Link to="/contacto">MUJER</Link>
     </li>
     <li className="centered-element">
       <Link to="/contacto">CALISTORE</Link>
     </li> */}
-    <div className="right-icons">
-      <li className="navbar-li">
-        <Link to="/update">
-          <MdOutlineManageAccounts />
-        </Link>
-      </li>
-      <li className="navbar-li">
-        <Link to="/content" className="carrito">
-          <div className="carrito-container">
-            <FaCartPlus />
-            <span className="cart-item-count">{cartItemCount}</span>
+          <div className="right-icons">
+            <li className="navbar-li">
+              <Link to="/update">
+                <MdOutlineManageAccounts />
+              </Link>
+            </li>
+            <li className="navbar-li">
+              <Link to="/content" className="carrito">
+                <div className="carrito-container">
+                  <FaCartPlus />
+                  <span className="cart-item-count">{cartItemCount}</span>
+                </div>
+              </Link>
+            </li>
           </div>
-        </Link>
-      </li>
-    </div>
-  </ul>
-</nav>
-</>
+        </ul>
+      </nav>
+    </>
   );
 };
